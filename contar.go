678deda02f184d3a0397e0a857a13c46b74c95ef
DjalmaFO <djalma.oliveira@intecomlogistica.com.br>
2021-02-23 09:58:49 -0300

    :rocket: Contagem até Nº 3 implementada

--- contar.go
+++ contar.go
@@ -7,7 +7,7 @@ import (
 
 // Contar123 exibe mensagem no console contando de 1 até 3
 func Contar123() {
-	for i := 1; i < 3; i++ {
+	for i := 1; i <= 3; i++ {
 		fmt.Println(i)
 		time.Sleep(time.Second)
 	}
